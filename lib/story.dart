c3b715a4b37f2d89aab85ab2cbbedd5e47508255
Dhruv Garg <gargdhruv732@gmail.com>
2021-06-21 00:38:40 +0530

    initial commit

---
+++ lib/story.dart
@@ -0,0 +1,14 @@
+// import 'package:flutter/material.dart';
+
+class Story{
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  Story({String storyTitle,String choice1,String choice2}){
+    this.storyTitle=storyTitle;
+    this.choice1=choice1;
+    this.choice2=choice2;
+  }
+}
+
